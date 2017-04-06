790a5a6a381a219e07751892ad8e3fe626351799
Jakub Rutkowski <jakub.rutkowski@tomtom.com>
2017-04-06 13:55:36 +0200

    EnableWebFlux annotation added

--- non-blocking-client/src/main/java/io/thingcare/NonBlockingClientApplication.java
+++ non-blocking-client/src/main/java/io/thingcare/NonBlockingClientApplication.java
@@ -2,11 +2,13 @@ package io.thingcare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
+@EnableWebFlux
 @SpringBootApplication
 public class NonBlockingClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NonBlockingClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NonBlockingClientApplication.class, args);
+    }
 }
